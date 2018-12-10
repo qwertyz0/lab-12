87bda8771f6dc1672f653f2b38b18dc311580d56
yn smol <ynis1998@gmail.com>
2018-12-10 21:51:42 +0300

    Add project files.

---
+++ lab 12/fMain.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace lab_12
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnMain = new System.Windows.Forms.Panel();
+            this.pnTools = new System.Windows.Forms.Panel();
+            this.btnLeftFar = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRightFar = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnDownFar = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnUpFar = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnCollapse = new System.Windows.Forms.Button();
+            this.btnExpand = new System.Windows.Forms.Button();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnCreateNew = new System.Windows.Forms.Button();
+            this.cbFigureType = new System.Windows.Forms.ComboBox();
+            this.cbCircle = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnTools.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnMain
+            // 
+            this.pnMain.BackColor = System.Drawing.Color.White;
+            this.pnMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnMain.Location = new System.Drawing.Point(0, 0);
+            this.pnMain.Name = "pnMain";
+            this.pnMain.Size = new System.Drawing.Size(639, 450);
+            this.pnMain.TabIndex = 0;
+            // 
+            // pnTools
+            // 
+            this.pnTools.Controls.Add(this.btnLeftFar);
+            this.pnTools.Controls.Add(this.btnLeft);
+            this.pnTools.Controls.Add(this.btnRightFar);
+            this.pnTools.Controls.Add(this.btnRight);
+            this.pnTools.Controls.Add(this.btnDownFar);
+            this.pnTools.Controls.Add(this.btnDown);
+            this.pnTools.Controls.Add(this.btnUpFar);
+            this.pnTools.Controls.Add(this.btnUp);
+            this.pnTools.Controls.Add(this.btnCollapse);
+            this.pnTools.Controls.Add(this.btnExpand);
+            this.pnTools.Controls.Add(this.btnHide);
+            this.pnTools.Controls.Add(this.btnShow);
+            this.pnTools.Controls.Add(this.btnCreateNew);
+            this.pnTools.Controls.Add(this.cbFigureType);
+            this.pnTools.Controls.Add(this.cbCircle);
+            this.pnTools.Controls.Add(this.label1);
+            this.pnTools.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnTools.Location = new System.Drawing.Point(378, 0);
+            this.pnTools.Name = "pnTools";
+            this.pnTools.Size = new System.Drawing.Size(261, 450);
+            this.pnTools.TabIndex = 1;
+            // 
+            // btnLeftFar
+            // 
+            this.btnLeftFar.Image = global::lab_12.Properties.Resources.Oxygen_Icons_org_Oxygen_Actions_arrow_left_double;
+            this.btnLeftFar.Location = new System.Drawing.Point(7, 265);
+            this.btnLeftFar.Name = "btnLeftFar";
+            this.btnLeftFar.Size = new System.Drawing.Size(40, 52);
+            this.btnLeftFar.TabIndex = 16;
+            this.btnLeftFar.UseVisualStyleBackColor = true;
+            this.btnLeftFar.Click += new System.EventHandler(this.btnLeftFar_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Image = global::lab_12.Properties.Resources.Oxygen_Icons_org_Oxygen_Actions_arrow_left;
+            this.btnLeft.Location = new System.Drawing.Point(53, 265);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(40, 52);
+            this.btnLeft.TabIndex = 15;
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnRightFar
+            // 
+            this.btnRightFar.Image = global::lab_12.Properties.Resources.Oxygen_Icons_org_Oxygen_Actions_arrow_right_double;
+            this.btnRightFar.Location = new System.Drawing.Point(218, 265);
+            this.btnRightFar.Name = "btnRightFar";
+            this.btnRightFar.Size = new System.Drawing.Size(40, 52);
+            this.btnRightFar.TabIndex = 14;
+            this.btnRightFar.UseVisualStyleBackColor = true;
+            this.btnRightFar.Click += new System.EventHandler(this.btnRightFar_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Image = global::lab_12.Properties.Resources.Oxygen_Icons_org_Oxygen_Actions_arrow_right;
+            this.btnRight.Location = new System.Drawing.Point(172, 265);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(40, 52);
+            this.btnRight.TabIndex = 13;
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnDownFar
+            // 
+            this.btnDownFar.Image = global::lab_12.Properties.Resources.Oxygen_Icons_org_Oxygen_Actions_arrow_down_double;
+            this.btnDownFar.Location = new System.Drawing.Point(99, 372);
+            this.btnDownFar.Name = "btnDownFar";
+            this.btnDownFar.Size = new System.Drawing.Size(67, 33);
+            this.btnDownFar.TabIndex = 12;
+            this.btnDownFar.UseVisualStyleBackColor = true;
+            this.btnDownFar.Click += new System.EventHandler(this.btnDownFar_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Image = global::lab_12.Properties.Resources.Oxygen_Icons_org_Oxygen_Actions_arrow_down;
+            this.btnDown.Location = new System.Drawing.Point(99, 333);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(67, 33);
+            this.btnDown.TabIndex = 11;
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnUpFar
+            // 
+            this.btnUpFar.Image = global::lab_12.Properties.Resources.Oxygen_Icons_org_Oxygen_Actions_arrow_up_double;
+            this.btnUpFar.Location = new System.Drawing.Point(99, 177);
+            this.btnUpFar.Name = "btnUpFar";
+            this.btnUpFar.Size = new System.Drawing.Size(67, 33);
+            this.btnUpFar.TabIndex = 10;
+            this.btnUpFar.UseVisualStyleBackColor = true;
+            this.btnUpFar.Click += new System.EventHandler(this.btnUpFar_Click);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Image = global::lab_12.Properties.Resources.Oxygen_Icons_org_Oxygen_Actions_arrow_up;
+            this.btnUp.Location = new System.Drawing.Point(99, 216);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(67, 33);
+            this.btnUp.TabIndex = 9;
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnCollapse
+            // 
+            this.btnCollapse.Image = global::lab_12.Properties.Resources.Icojam_Blue_Bits_Math_minus;
+            this.btnCollapse.Location = new System.Drawing.Point(99, 294);
+            this.btnCollapse.Name = "btnCollapse";
+            this.btnCollapse.Size = new System.Drawing.Size(67, 33);
+            this.btnCollapse.TabIndex = 8;
+            this.btnCollapse.UseVisualStyleBackColor = true;
+            this.btnCollapse.Click += new System.EventHandler(this.btnCollapse_Click);
+            // 
+            // btnExpand
+            // 
+            this.btnExpand.Image = global::lab_12.Properties.Resources.Awicons_Vista_Artistic_Add;
+            this.btnExpand.Location = new System.Drawing.Point(99, 255);
+            this.btnExpand.Name = "btnExpand";
+            this.btnExpand.Size = new System.Drawing.Size(67, 33);
+            this.btnExpand.TabIndex = 7;
+            this.btnExpand.UseVisualStyleBackColor = true;
+            this.btnExpand.Click += new System.EventHandler(this.btnExpand_Click);
+            // 
+            // btnHide
+            // 
+            this.btnHide.Location = new System.Drawing.Point(22, 107);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(192, 23);
+            this.btnHide.TabIndex = 5;
+            this.btnHide.Text = "Приховати об\'єкт";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(22, 136);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(192, 23);
+            this.btnShow.TabIndex = 4;
+            this.btnShow.Text = "Показати об\'єкт";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnCreateNew
+            // 
+            this.btnCreateNew.Location = new System.Drawing.Point(22, 78);
+            this.btnCreateNew.Name = "btnCreateNew";
+            this.btnCreateNew.Size = new System.Drawing.Size(110, 23);
+            this.btnCreateNew.TabIndex = 3;
+            this.btnCreateNew.Text = "Новий об\'єкт";
+            this.btnCreateNew.UseVisualStyleBackColor = true;
+            this.btnCreateNew.Click += new System.EventHandler(this.btnCreateNew_Click);
+            // 
+            // cbFigureType
+            // 
+            this.cbFigureType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFigureType.FormattingEnabled = true;
+            this.cbFigureType.Items.AddRange(new object[] {
+            "Коло",
+            "Прямокутник",
+            "Трикутник"});
+            this.cbFigureType.Location = new System.Drawing.Point(138, 80);
+            this.cbFigureType.Name = "cbFigureType";
+            this.cbFigureType.Size = new System.Drawing.Size(76, 21);
+            this.cbFigureType.TabIndex = 2;
+            // 
+            // cbCircle
+            // 
+            this.cbCircle.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCircle.FormattingEnabled = true;
+            this.cbCircle.Location = new System.Drawing.Point(22, 30);
+            this.cbCircle.Name = "cbCircle";
+            this.cbCircle.Size = new System.Drawing.Size(192, 21);
+            this.cbCircle.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Перелік об\'єктів";
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 450);
+            this.Controls.Add(this.pnTools);
+            this.Controls.Add(this.pnMain);
+            this.Name = "fMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабораторна робота №12";
+            this.pnTools.ResumeLayout(false);
+            this.pnTools.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnMain;
+        private System.Windows.Forms.Panel pnTools;
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnCreateNew;
+        private System.Windows.Forms.ComboBox cbFigureType;
+        private System.Windows.Forms.ComboBox cbCircle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLeftFar;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRightFar;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnDownFar;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnUpFar;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnCollapse;
+        private System.Windows.Forms.Button btnExpand;
+    }
+}
+
